3b620be7c57801881338029605fa6bce08416801
Vasilis Valatsos <vasilvalat@gmail.com>
2024-06-14 03:49:38 +0200

    Auto theme switcher on login

---
+++ .themes/orzhovtheme.sh
@@ -0,0 +1,5 @@
+xfconf-query -c xsettings -p /Gtk/MonospaceFontName -s 'MesloLGS NF Regular 12'
+xfconf-query -c xsettings -p /Gtk/FontName -s 'Noto Sans 12'
+xfconf-query -c xsettings -p /Gtk/CursorThemeName -s default
+xfconf-query -c xsettings -p /Net/IconThemeName -s GNOME
+xfconf-query -c xsettings -p /Net/ThemeName -s Adwaita-dark
