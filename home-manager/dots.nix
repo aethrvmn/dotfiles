d0bbd4d21d3367807fb5755e416718a369ee40b1
Vasilis Valatsos <vasilvalat@gmail.com>
2024-07-21 22:20:22 +0200

    trying to make flake.nix work

--- home-manager/dots.nix
+++ home-manager/dots.nix
@@ -1,12 +1,9 @@
 { config, ... }:
 
-let
-  dotdir = ".";
-in
 {
   # Raw config files for the programs that Nix doesn't support yet
-  home.file.".config/labwc".source = dotdir+"/labwc";
+  home.file.".config/labwc".source = "./labwc";
 
   # This is a custom theme I'm working on for my system
-  home.file.".themes/Orzhov".source = dotdir+"/Orzhov";
+  home.file.".themes/Orzhov".source = "./Orzhov";
 }
